b1c0ba5289060d7b2022b24849fd39520e85e951
Pascal Brand <pascal.brand38@gmail.com>
2026-03-10 06:44:39 +0100

    pascal-git-clone
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- help/alias.bash
+++ help/alias.bash
@@ -3,3 +3,13 @@ function pascal-git-fetch-pr() {
 }
 
 alias help='~/dev/pascal-brand38/pascal-brand38/help/help.bash'
+
+function pascal-git-clone() {
+  REPO=$1
+  OWNER=$2
+  if [ -z "$OWNER" ]; then
+    OWNER="pascal-brand38"
+  fi
+  git clone git@github.com:$OWNER/$REPO.git
+  cd $REPO
+}
