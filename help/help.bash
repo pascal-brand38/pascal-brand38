603b783b8f831f424128d73254b6e64eebc3c9e8
Pascal Brand <pascal.brand38@gmail.com>
2026-04-10 16:23:19 +0200

    fix: help/help.bash
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- help/help.bash
+++ help/help.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ $# -eq 0 ]; then
-  for f in ~/dev/pascal-brand38/pascal-brand38/help/*.help; do
+  for f in ~/dev/pascal-brand38/pascal-brand38/help/help/*.help; do
     export filenodir="${f##*/}"
     export fileext="${filenodir%.*}"
     printf 'help %s\n' "${fileext}"
   done
 else
-  cat ~/dev/pascal-brand38/pascal-brand38/help/"$1".help
+  cat ~/dev/pascal-brand38/pascal-brand38/help/help/"$1".help
 fi
